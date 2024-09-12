e0623fe34731b1f6b45220b972c409c3a08781fd
unknown <isiaqayandeji@gmail.com>
2024-09-12 00:43:38 -0700

    initial commit

---
+++ calc.py
@@ -0,0 +1,24 @@
+Addition
+x = int(input("Enter your first number"))
+y = int(input("Enter your second nunmer"))
+result = x + y 
+print(result)
+
+
+Subtraction
+x = int(input("Enter your first number"))
+y = int(input("Enter your second number"))
+result = x - y
+print(result)
+
+Multiplication
+x = int(input("Enter your first number"))
+y = int(input("Enter your second number"))
+result = x x y
+print(result)
+
+Division
+x = int(input("Enter your first numner"))
+y = int(input("Enter your second number"))
+result = x / y
+print(result)
---
+++ variable.py
@@ -0,0 +1,8 @@
+ahmed = "you are foolish"
+rokeeb = "you are mad"
+age2 = 50
+age = 54
+print(ahmed)
+print(rokeeb)
+print(age2)
+print(age)
